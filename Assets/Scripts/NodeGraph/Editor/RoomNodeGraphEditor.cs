7c4aa40b43589fca42312792d042c826cb2e96dc
Jakub Kulewicz <Jakub.Kulewicz@unit4.com>
2022-05-17 17:17:43 +0200

    Add basic Editor

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -5,10 +5,41 @@ using UnityEditor;
 
 public class RoomNodeGraphEditor : EditorWindow
 {
+    private GUIStyle roomNodeStyle;
+    
+    // Layout for node variables
+    private const int nodePadding = 20; // Spacing inside the GUI element
+    private const int nodeBorder = 12; // Spacing outside the GUI element
+
     [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
     public static void OpenWindow()
     {
         // Get existing open window or if none, make a new one:
         GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+
+    private void OnEnable(){
+        roomNodeStyle = new GUIStyle();
+        roomNodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node1.png") as Texture2D;
+        roomNodeStyle.normal.textColor = Color.white;
+        roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
+        roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+    }
+
+    // <summary>
+    // The graph that is being edited
+    // </summary>
+    private void OnGUI()
+    {
+        // Test log
+        //Debug.Log("Room Node Graph Editor called");
+
+        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(160f, 75f)), roomNodeStyle);
+        EditorGUILayout.LabelField("Node 1");
+        GUILayout.EndArea();
+
+        GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(160f, 75f)), roomNodeStyle);
+        EditorGUILayout.LabelField("Node 2");
+        GUILayout.EndArea();
     }
 }
