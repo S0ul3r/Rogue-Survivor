625f49e9d2d612ee7e9c0b235b33b6909d3be401
S0ul3r <jakubkulewicz05@gmail.com>
2022-11-05 13:45:50 +0100

    spacing fix Textmesh pro refresh

--- Assets/Scripts/Dungeon/Doorway.cs
+++ Assets/Scripts/Dungeon/Doorway.cs
@@ -10,17 +10,24 @@ public class Doorway
     #region Header
     [Header("Upper left pos of doorway to start copying from")]
     #endregion
+
     public Vector2Int doorwayStartCopyPos;
+    
     #region Header
     [Header("Width of doorway to copy over")]
     #endregion
+    
     public int doorwayCopyWidth;
+    
     #region Header
     [Header("Height of doorway to copy over")]
     #endregion
+    
     public int doorwayCopyHeight;
+    
     [HideInInspector]
     public bool isConnected = false;
+
     [HideInInspector]
     public bool isUnavailable = false;
 }
